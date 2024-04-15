8d12a79c971e6f1b15b2b1c3e4dd1e285cfda7c0
Lorenzo Broch <160625132+LorenzoBroch@users.noreply.github.com>
2024-04-15 12:48:52 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,26 @@
+function calcular() {
+    var numero1 = parseInt(document.getElementById('numero1').value);
+    var numero2 = parseInt(document.getElementById('numero2').value);
+    var operacao = (document.getElementById('escolha').value);
+    var texto = document.querySelector(".resultado")
+
+switch(operacao) {
+    case 'Adicao':
+        resultado = numero1 + numero2;
+        console.log(resultado);
+        break
+    case 'Subtracao':
+        resultado = numero1 - numero2;
+        console.log(resultado);
+        break
+    case 'Multiplicacao':
+        resultado = numero1 * numero2;
+        console.log(resultado);
+        break
+    case 'Divisao':
+        resultado = numero1 / numero2;
+        console.log(resultado);
+        break
+}
+texto.textContent = resultado;
+}
